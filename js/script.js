17d3e98c470d7977656ca7b0107594d2822d5a9d
Ousbin <kaneousmane426@gmail.com>
2021-12-28 16:44:33 +0000

    portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:[""," Designer web","Développeur web","Développeur mobile"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
