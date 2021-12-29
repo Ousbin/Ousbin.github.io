c6d01442b7ce531782ffde5aae8bb907fcfb28a3
Ousbin <kaneousmane426@gmail.com>
2021-12-29 11:51:14 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:[""," Designer web","Développeur web","Développeur mobile"],
+    strings:[""," Designer web","Développeur web/mobile"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
